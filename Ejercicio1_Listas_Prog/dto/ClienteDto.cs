e3f9c659e7973759a838389bacc0a5677a1b52e8
DESKTOP-TRFJKS9\Rubén Bernal Ramos <csi22-rbermos@colegioaltair.net>
2023-11-14 13:59:27 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio1_Listas_Prog/dto/ClienteDto.cs
@@ -0,0 +1,37 @@
+﻿//Ruben Bernal Ramos, CSI1
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1_Listas_Prog.dto
+{
+    /// <summary>
+    /// Clase que utilizaré como tipo objeto para añadir los clientes
+    /// rbr - 141123
+    /// </summary>
+    internal class ClienteDto
+    {
+        //Declaro las variables
+        string nombreCliente;
+        string apellidosCliente;
+        string dniCliente;
+        string fchNacimientoCliente;
+        string emailCliente;
+        int tlfCliente;
+        string fchAltaCliente;
+        string fchBajaCliente;
+
+        //Getters y Setters
+        public string NombreCliente { get => nombreCliente; set => nombreCliente = value; }
+        public string ApellidosCliente { get => apellidosCliente; set => apellidosCliente = value; }
+        public string DniCliente { get => dniCliente; set => dniCliente = value; }
+        public string FchNacimientoCliente { get => fchNacimientoCliente; set => fchNacimientoCliente = value; }
+        public string EmailCliente { get => emailCliente; set => emailCliente = value; }
+        public int TlfCliente { get => tlfCliente; set => tlfCliente = value; }
+        public string FchAltaCliente { get => fchAltaCliente; set => fchAltaCliente = value; }
+        public string FchBajaCliente { get => fchBajaCliente; set => fchBajaCliente = value; }
+    }
+}
---
+++ Ejercicio1_Listas_Prog/servicios/MenuImplementacion.cs
@@ -0,0 +1,32 @@
+﻿//Ruben Bernal Ramos, CSI1
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1_Listas_Prog.servicios
+{
+    /// <summary>
+    /// Implementación de la interfaz Menu
+    /// rbr - 141123
+    /// </summary>
+    internal class MenuImplementacion : MenuInterfaz
+    {
+        /// <summary>
+        /// Método que mostrará el menú por consola
+        /// rbr - 141123 
+        /// </summary>
+        public void menu()
+        {
+            //Muestro el menú por consola
+            Console.WriteLine("#########################");
+            Console.WriteLine("##########MENÚ###########");
+            Console.WriteLine("#########################");
+            Console.WriteLine("1. Cerrar                ");
+            Console.WriteLine("2. Registrar cliente     ");
+            Console.WriteLine("#########################");
+        }
+    }
+}
